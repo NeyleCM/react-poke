7af6d4f0c59630180b3cf29c6f34cd7a15c3a976
NeyleCM <neylecmorantes@gmail.com>
2024-11-02 16:02:28 +0100

    useEffect y loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+/* -----CON useEffect----- */
+import { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import SearchForm from './SearchForm';
@@ -6,42 +7,109 @@ import PokemonDetails from './PokemonDetails';
 import ErrorMessage from './ErrorMessage';
 
 function App() {
-  const [pokemon, setPokemon] = useState(null);
-  const [pokemonName, setPokemonName] = useState('');
-  const [error, setError] = useState(null);
+  const [pokemon, setPokemon] = useState(null); 
+  const [pokemonName, setPokemonName] = useState(''); 
+  const [error, setError] = useState(null); 
+  const [loading, setLoading] = useState(false)
 
-  const fetchPokemon = async (name) => {
-    try {
-      setError(null);
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
-      if (!response.ok) {
-        throw new Error('Pokémon no encontrado');
+    const fetchPokemon = async () => {
+      try {
+        setError(null);
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`);
+        
+        if (!response.ok) {
+          throw new Error('Pokémon no encontrado');
+        }
+
+        const data = await response.json();
+        setPokemon(data); 
+      } catch (err) {
+        setError(err.message); 
+        setPokemon(null); 
       }
-      const data = await response.json();
-      setPokemon(data);
-    } catch (err) {
-      setError(err.message);
-      setPokemon(null);
-    }
-  };
+      finally {
+        setLoading(false)
+      }
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetchPokemon(pokemonName);
-  };
+    useEffect(() => {
+      setLoading(true)
+      const delay = setTimeout(() => {
+        if (pokemonName) {
+          fetchPokemon(pokemonName); 
+        }
+      }, 500)
+      
+      return () => clearTimeout(delay)
+    }, [pokemonName])
+  
+    const handleChange = (e) => {
+      setPokemonName(e.target.value); 
+    };
 
-  return (
-    <>
-      <Header />
-      <SearchForm 
-        pokemonName={pokemonName} 
-        setPokemonName={setPokemonName} 
-        handleSubmit={handleSubmit} 
-      />
-      {error && <ErrorMessage error={error} />}
-      {pokemon && <PokemonDetails pokemon={pokemon} />}
-    </>
-  );
+    return (
+      <>
+        <Header />
+        <SearchForm 
+          pokemonName={pokemonName} 
+          setPokemonName={setPokemonName} 
+          handleChange={handleChange} 
+        />
+        {loading && <div className='spinner'></div>}
+        {error && <p style={{color: 'red', fontWeight: 'bold'}}>{error}</p>}
+        {pokemon && <PokemonDetails pokemon={pokemon} />}
+      </>
+    );
 }
 
 export default App;
+
+/*----SIN useEffect-----*/
+
+// import { useState } from 'react';
+// import './App.css';
+// import Header from './Header';
+// import SearchForm from './SearchForm';
+// import PokemonDetails from './PokemonDetails';
+// import ErrorMessage from './ErrorMessage';
+
+// function App() {
+//   const [pokemon, setPokemon] = useState(null);
+//   const [pokemonName, setPokemonName] = useState('');
+//   const [error, setError] = useState(null);
+
+//   const fetchPokemon = async (name) => {
+//     try {
+//       setError(null);
+//       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
+//       if (!response.ok) {
+//         throw new Error('Pokémon no encontrado');
+//       }
+//       const data = await response.json();
+//       setPokemon(data);
+//     } catch (err) {
+//       setError(err.message);
+//       setPokemon(null);
+//     }
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     fetchPokemon(pokemonName);
+//   };
+
+//   return (
+//     <>
+//       <Header />
+//       <SearchForm 
+//         pokemonName={pokemonName} 
+//         setPokemonName={setPokemonName} 
+//         handleSubmit={handleSubmit} 
+//       />
+//       {error && <ErrorMessage error={error} />}
+//       {pokemon && <PokemonDetails pokemon={pokemon} />}
+//     </>
+//   );
+// }
+
+// export default App;
--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,14 +1,34 @@
-const SearchForm = ({ pokemonName, setPokemonName, handleSubmit }) => (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="pokemonName">Buscar Pokémon:</label>
-      <input
-        type="text"
-        id="pokemonName"
-        value={pokemonName}
-        onChange={(e) => setPokemonName(e.target.value)}
-      />
-      <button type="submit">Buscar</button>
-    </form>
-  );
+/*----CON useEffect-----*/
+const SearchForm = ({ pokemonName, handleChange }) => (
+  <>
+  <form onSubmit={(e) => e.preventDefault()}> {/* Previene el comportamiento por defecto */}
+    <label htmlFor="pokemonName">Buscar Pokémon:</label>
+    <input
+      type="text"
+      id="pokemonName"
+      value={pokemonName}
+      placeholder="Nombre del pokemón"
+      onChange={handleChange} // Cambia el valor de pokemonName en tiempo real
+    />
+  </form>
+  </>
+);
+
+export default SearchForm;
+
+/*----SIN useEffect-----*/
+
+// const SearchForm = ({ pokemonName, setPokemonName, handleSubmit }) => (
+//     <form onSubmit={handleSubmit}>
+//       <label htmlFor="pokemonName">Buscar Pokémon:</label>
+//       <input
+//         type="text"
+//         id="pokemonName"
+//         value={pokemonName}
+//         onChange={(e) => setPokemonName(e.target.value)}
+//       />
+//       <button type="submit">Buscar</button>
+//     </form>
+//   );
   
-  export default SearchForm;
+//   export default SearchForm;
